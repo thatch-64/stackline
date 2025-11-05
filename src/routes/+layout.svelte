db577bfee000c0a77cd075bc1c2c47adccf0a966
thatch-64 <thatch.64.dev@gmail.com>
2025-11-04 22:54:19 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+
+	let { children } = $props();
+
+
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav>
+	<ul class="flex gap-4 p-4 bg-gray-800 text-white">
+		<li><a href="/" class="hover:underline">Dashboard</a></li>
+		<li><a href="/projects" class="hover:underline">Projects</a></li>
+		<li><a href="/tasks" class="hover:underline">Tasks</a></li>
+	</ul>
+</nav>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
+
+{@render children()}
